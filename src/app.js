e4e674c9d5fcb5b55fcb9b04619910d8a16d1c24
rafaelfb344355 <rafael344355@gmail.com>
2024-03-24 13:33:03 -0300

    On branch main  Changes to be committed:

--- src/app.js
+++ src/app.js
@@ -1,42 +1,111 @@
-var express = require('express'),
-bodyParser = require('body-parser'),
-cors = require('cors'),
-mongoose = require('mongoose');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+const seu_usuario = 'rafael344355';
+const senha = 'ndTwvS5JFefCMrBd';
+const poitTuristicRoutes = express.Router();
 
 const Port = process.env.PORT || 3001;
 
+// Definindo o esquema para o ponto turístico
+const PoitTuristicSchema = new mongoose.Schema({
+  Nome: String,
+  descricao: String, // Corrigido o nome do campo
+  horario: String,
+  valoringresso: String,
+  picture1: String,
+  picture2: String,
+  picture3: String,
+  lon: String,
+  lat: String
+}, {
+  collection: 'PoitTuristic'
+});
+
+const PoitTuristic = mongoose.model('PoitTuristic', PoitTuristicSchema);
+
+// api para adicionar ponto turístico
+poitTuristicRoutes.route('/add').post(function (req, res) {
+  let poitTuristic = new PoitTuristic(req.body);
+  poitTuristic.save()
+    .then(() => {
+      res.status(200).json({'status': 'success','mssg': 'Ponto turístico adicionado com sucesso'});
+    })
+    .catch(err => {
+      res.status(409).send({'status': 'failure','mssg': 'Não foi possível salvar no banco de dados'});
+    });
+});
+
+// api para obter todos os pontos turísticos
+poitTuristicRoutes.route('/').get(function (req, res) {
+  PoitTuristic.find(function (err, poitTuristics) {
+    if(err){
+      res.status(400).send({'status': 'failure','mssg': 'Algo deu errado'});
+    }
+    else {
+      res.status(200).json({'status': 'success','poitTuristics': poitTuristics});
+    }
+  });
+});
+
+// api para obter ponto turístico por nome
+poitTuristicRoutes.route('/name/:name').get(function (req, res) {
+  let name = req.params.name;
+  PoitTuristic.find({ Nome: name }, function(err, poitTuristics) {
+    if (err) {
+      res.status(500).send('Erro interno do servidor');
+    } else if (poitTuristics.length === 0) {
+      res.status(404).send('Nenhum ponto encontrado');
+    } else {
+      res.status(200).json(poitTuristics);
+    }
+  });
+});
+
+// api para obter ponto turístico por ID
+poitTuristicRoutes.route('/:id').get(function (req, res) {
+  let id = req.params.id;
+  PoitTuristic.findById(id, function (err, poitTuristic){
+    if(err){
+      res.status(400).send({'status': 'failure','mssg': 'Algo deu errado'});
+    }
+    else {
+      res.status(200).json({'status': 'success','poitTuristic': poitTuristic});
+    }
+  });
+});
+
+// api para excluir ponto turístico
+poitTuristicRoutes.route('/delete/:id').delete(function (req, res) {
+  PoitTuristic.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.status(400).send({'status': 'failure','mssg': 'Algo deu errado'});
+    }
+    else {
+      res.status(200).json({'status': 'success','mssg': 'Excluído com sucesso'});
+    }
+  });
+});
+
 mongoose.Promise = global.Promise;
-// vai mudar o ip
-mongoose.connect('mongodb+srv://rafael344355:ndTwvS5JFefCMrBd@boraalidb.paquwbm.mongodb.net/', { useNewUrlParser: true , useUnifiedTopology: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)});
-
-const PoitTuristicRoute = require('../routes/poitTuristic.route');
-const Ponto = require('../model/PoitTuristic');
-
-const UserRoute = require('../routes/user.route');
-const User= require('../model/User');
-
+// Conectar ao banco de dados MongoDB
+mongoose.connect('mongodb+srv://rafael344355:ndTwvS5JFefCMrBd@boraalidb.paquwbm.mongodb.net/sua_database', { useNewUrlParser: true , useUnifiedTopology: true }).then(
+  () => {console.log('Banco de dados conectado') },
+  err => { console.log('Não foi possível conectar ao banco de dados'+ err)}
+);
 
 var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use('/',
+  console.log('hello world')
+ );
+// Rota principal
+app.use('/poitTuristic', poitTuristicRoutes);
 
-
-app.use('/poitTuristic', PoitTuristicRoute);
-app.use('/user', UserRoute);
-
-
-
-app.get('/', PoitTuristicRoute);
-app.get("/poitTuristic/:id",PoitTuristicRoute);
-app.get("/poitTuristic/:name",PoitTuristicRoute);
-
-app.get('/', UserRoute);
-app.get("/user/:id",UserRoute);
-
-
-app.listen(Port,function(){
-    console.log('Listening on port ' + Port);
+app.listen(Port, function(){
+    console.log('Escutando na porta ' + Port);
 });
